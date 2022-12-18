82404ee6d83aa84dcb48a84faa628132ecfb781f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-18 10:12:58 +0000

    Adding tarfile member sanitization to extractall()

--- test/test_boostnote.py
+++ test/test_boostnote.py
@@ -12,7 +12,26 @@ def extract_file(path):
     filename = os.path.join(os.path.dirname(__file__), 'sample', 'boostnote.tar.gz')
 
     with tarfile.open(filename, 'r:gz') as fp:
-        fp.extractall(path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(fp, path)
 
 
 class TestBoostnote(unittest.TestCase):
